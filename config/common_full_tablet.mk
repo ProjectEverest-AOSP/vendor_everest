MODEDGES24/android_vendor_everest

vendor: Refactor for EverestOS
Signed-off-by: janak <niraulajanak2019@gmail.com>

config/common_full_tablet.mk
@@ -1,14 +1,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
-# Inherit full common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full.mk)
+# Inherit full common Everest stuff
+$(call inherit-product, vendor/everest/config/common_full.mk)
 
 # Required packages
 PRODUCT_PACKAGES += \
     LatinIME
 
-# Include Lineage LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
+# Include Everest LatinIME dictionaries
+PRODUCT_PACKAGE_OVERLAYS += vendor/everest/overlay/dictionaries
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/everest/overlay/dictionaries
 
-$(call inherit-product, vendor/lineage/config/telephony.mk)
+$(call inherit-product, vendor/everest/config/telephony.mk)

ProjectEverest-AOSP/vendor_everest

config/common_full_tablet.mk
@@ -1,7 +1,6 @@
-# Inherit mobile full common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
+# Inherit mobile full common Everest stuff
+$(call inherit-product, vendor/everest/config/common_mobile_full.mk)
 
-# Inherit tablet common Lineage stuff
-$(call inherit-product, vendor/lineage/config/tablet.mk)
-
-$(call inherit-product, vendor/lineage/config/telephony.mk)
+# Inherit tablet common Everest stuff
+$(call inherit-product, vendor/everest/config/tablet.mk)
+$(call inherit-product, vendor/everest/config/telephony.mk)